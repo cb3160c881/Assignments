6c0eddb354c2ec09f29d5332662d157d30ed8618
Chaitanya Bhargav <chaitanya.bhargav19@gmail.com>
2025-09-27 03:53:42 -0700

    Assignment - 1 DataTypes

---
+++ src/language/basics/DataTypesAssignment.java
@@ -0,0 +1,93 @@
+package language.basics;
+
+public class DataTypesAssignment {
+
+	public static void main(String[] args) {
+
+		//1. Temperature of a city in degrees Celsius: 25.5
+		float temparature=25.5f;
+		System.out.println("Temperature of a city is: "+temparature);
+
+		//2. Whether a customer has placed an order: true or false
+		boolean order=true;
+		System.out.println("Customer Order has Placed: "+order);
+
+		//3. Person's phone number: "123-456-7890"
+		String phonenumber="123-456-7890";
+		System.out.println("Person Phone Number is: "+phonenumber);	
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float amount=1000.50f;
+		System.out.println("Customer Bank Balance: "+amount);
+		
+		//5. Person's email address: "john.doe@example.com"
+		String email="john.doe@example.com";
+		System.out.println("Person Email Address is: "+email);
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude=37.7749f;
+		float longitude=-122.4194f;
+		System.out.println("Location Coordinates is: "+latitude+", "+longitude);
+		
+		//7. Person's marital status: true or false
+		boolean maritalstatus=true;
+		System.out.println("Person Marital Status is: "+maritalstatus);
+		
+		//8. Person's occupation: "Software Engineer"
+		String occupation="Software Engineer";
+		System.out.println("Person Occupation is: "+occupation);
+		
+		//9. Person's Favorite color: "Blue"
+		String colour="Blue";
+		System.out.println("Favorite Color is: "+colour);
+		
+		//10.Current year: 2023
+		short year=2023;
+		System.out.println("Current Year is: "+year);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		int followers = 1000000;
+		System.out.println("Social Media Followers: "+followers);
+		
+		//12.Rating of a movie: 7.5
+		float rating=7.5f;
+		System.out.println("Movie Rating is: "+rating );
+		
+		//13.Person's blood type: 'A'
+		char bloodtype='A';
+		System.out.println("Blood Type is: "+bloodtype);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String book="To Kill a Mockingbird";
+		System.out.println("Title of a book: "+book);
+		
+		//15.Number of employees in a company: 500
+		short employees =500;
+		System.out.println("Number of Employees: "+employees);
+		
+		//16.Time of an event: 2:30 PM
+		String time="2:30 PM";
+		System.out.println("Time of an event: "+time);
+		
+		//17.Name of a country: "United States"
+		String country="United States";
+		System.out.println("Country: "+country);
+		
+		//18.Person's eye color: "Brown"
+		String eyecolor="Brown";
+		System.out.println("Eye Color: "+eyecolor);
+		
+		//19.Person's birthplace: "New York City"
+		String birthplace="New York City";
+		System.out.println("Birth Place: "+birthplace);
+		
+		//20.Distance between two cities: 200.5
+		float distance=200.5f;
+		System.out.println("Distance between Cities: "+distance);
+		
+		
+
+
+	}
+
+}
